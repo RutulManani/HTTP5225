6a5ee75fb3b0dcaf29435dee5590bcdcaee64fa2
Rutul Manani <rutulmanani18@gmail.com>
2025-05-14 16:35:05 -0400

    Week 2 PHP variables

---
+++ Week2/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 2</title>
+</head>
+<body>
+ <!--- Learning Echo --->
+<?php
+
+echo "hello";
+
+echo "<p>I'm Rutul</p>";
+
+?>
+
+<hr>
+
+<!--- Learning Variables --->
+<?php
+
+$fname = "Rutul";
+$lname = "Manani";
+
+echo "<h1>Hello $fname $lname</h1>";
+
+?>
+
+
+
+
+</body>
+</html>
